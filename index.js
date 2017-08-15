396637646e222303f80ebdd6924d077853fc8f64
root <root@s3-node-vanilla-1.na.scality.cloud>
2017-08-15 18:23:45 +0000

    hook data streams

--- index.js
+++ index.js
@@ -119,10 +119,42 @@ mdServer.startServer();
 
 // data
 
+class MemcachedFileStore extends arsenal.storage.data.file.DataFileStore {
+    constructor(dataConfig, logApi) {
+	super(dataConfig, logApi);
+	console.log('filestore constructor');
+    }
+
+    setup(callback) {
+	console.log('data setup');
+	callback(null);
+    }
+
+    put(dataStream, size, log, callback) {
+	console.log('data put');
+    }
+
+    stat(key, log, callback) {
+	console.log('data stat');
+    }
+
+    get(key, byteRange, log, callback) {
+	console.log('data get');
+    }
+
+    delete(key, log, callback) {
+	console.log('data delete');
+    }
+
+    getDiskUsage(callback) {
+	console.log('data getDiskUsage');
+    }
+}
+
 const dataServer = new arsenal.network.rest.RESTServer(
     { bindAddress: '0.0.0.0',
       port: 9991,
-      dataStore: new arsenal.storage.data.file.DataFileStore(
+      dataStore: new MemcachedFileStore(
           { dataPath: '/tmp',
             log: logOptions }),
       log: logOptions });
@@ -133,6 +165,7 @@ dataServer.setup(err => {
                      { error: err });
         return;
     }
+
     dataServer.start();
 });
 
